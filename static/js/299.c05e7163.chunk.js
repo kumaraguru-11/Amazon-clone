d99aa0a44d2d6586be57aeb332e6d335d6496c15
kumaraguru-11 <kumaraguru1181@gmail.com>
2024-03-26 18:51:12 -0200

    Updates

---
+++ static/js/299.c05e7163.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkamazon_clone=self.webpackChunkamazon_clone||[]).push([[299],{9155:(e,s,t)=>{t.r(s),t.d(s,{default:()=>l});t(5043);const a=t.p+"static/media/404 error.b1a9347db45726073936.gif";var c=t(5475),i=t(579);const l=()=>(0,i.jsxs)("div",{className:"d-flex flex-column align-items-center",style:{height:"100%",width:"100%"},children:[(0,i.jsx)("img",{src:a,alt:"Error Img",height:400,width:400}),(0,i.jsx)(c.N_,{to:"/",children:"Back to Home"})]})}}]);
+//# sourceMappingURL=299.c05e7163.chunk.js.map
